816f0e2acd52133cf264babd2f42b85c2e6a58a8
Pascal Andersson <pascal.andersson@soprasteria.com>
2022-04-14 14:40:08 +0200

    Testing uploading images.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [passaDig, setPassadig] = useState(null);
+
+  useEffect(() => {
+    const initiate = async () => {
+      await initStates();
+    }
+
+    initiate();
+  }, [])
+
+  const initStates = async () => {
+    setPassadig(createPassadigItem());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        {
+          !passaDig ?
+            <p>Loading</p>
+            :
+            <div>
+              <h1>{passaDig.name} {passaDig.quote}</h1>
+              <img src={passaDig.image} />
+            </div>
+        }
+      </div>
     </div>
   );
 }
+
+const createPassadigItem = () => {
+  const randomPassadigItem = getRandomPassadigItem();
+  const randomQuote = getRandomQuote();
+
+  var item = {
+    name: randomPassadigItem.name,
+    image: randomPassadigItem.image,
+    quote: randomQuote
+  }
+
+  return item;
+}
+
+const getRandomPassadigItem = () => {
+  const randomNumber = Math.floor(Math.random() * 14);
+
+  const items = getPassadigItems();
+  return items[randomNumber];
+}
+
+const getRandomQuote = () => {
+  const randomNumber = Math.floor(Math.random() * 5);
+
+  const items = getPassadigQuotes();
+  return items[randomNumber];
+}
+
+const getPassadigItems = () => {
+  const items = [
+    {
+      name: 'Balogh',
+      image: '/img/balogh.png'
+    },
+    {
+      name: 'Balogh',
+      image: '/img/balogh2.png'
+    },
+    {
+      name: 'Balogh',
+      image: '/img/balogh3.png'
+    },
+    {
+      name: 'David',
+      image: '/img/david.png'
+    },
+    {
+      name: 'David',
+      image: '/img/david2.png'
+    },
+    {
+      name: 'Erik',
+      image: '/img/erik.png'
+    },
+    {
+      name: 'Love',
+      image: '/img/love.png'
+    },
+    {
+      name: 'Love',
+      image: '/img/love2.png'
+    },
+    {
+      name: 'Oscar',
+      image: '/img/oscar.png'
+    },
+    {
+      name: 'Oscar',
+      image: '/img/oscar2.png'
+    },
+    {
+      name: 'Pascal',
+      image: '/img/pascal.png'
+    },
+    {
+      name: 'Pascal',
+      image: '/img/pascal2.png'
+    },
+    {
+      name: 'Pascal',
+      image: '/img/pascal3.png'
+    },
+    {
+      name: 'Mattias',
+      image: '/img/mattias.png'
+    },
+    {
+      name: 'Mattias',
+      image: '/img/mattias2.png'
+    },
+  ]
+
+  return items;
+}
+
+const getPassadigQuotes = () => {
+  const quotes = [
+    'ber dig vänligen att PASSA DIG!!!',
+    'tycker starkt att du ska ta och PASSA DIG!!!',
+    'tycker att du gör bäst i att PASSA DIG snarast!',
+    'insisterar att du nu måste PASSA DIG!!!',
+    'undviker helst att bli förbannad. Så du måste PASSA DIG!!!',
+    'håller med resten och säger: PASSA DIG!!!',
+    ': Passa dig!'
+  ]
+
+  return quotes;
+}
+
+
 
 export default App;
